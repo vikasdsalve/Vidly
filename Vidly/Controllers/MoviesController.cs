4126e3dc0ce1ed70f1afad1e6fce428fc11c2e32
Vikas Salve <Vikas.Dsalve@hotmail.com>
2020-04-24 10:19:05 +0530

    10. Action Results

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,6 +14,10 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Sherk!" };
             return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1 , sortBy ="name" });
         }
     }
 }
