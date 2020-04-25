3efb5d8b0888efed76735098d5683320be5449a6
Vikas Salve <Vikas.Dsalve@hotmail.com>
2020-04-25 23:00:19 +0530

    52. Data Annotations

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,7 +10,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please Enter Customer's Name")]
         [StringLength(255)]
         public string Name { get; set; }
 
