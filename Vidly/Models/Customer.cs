59de24cdb6ac194f36611bb017020bbaec8094a6
Vikas Salve <Vikas.Dsalve@hotmail.com>
2020-04-25 11:32:02 +0530

    28. Overriding Conventions

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedtoNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
