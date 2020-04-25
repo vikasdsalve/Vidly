583440fd3b9bc5130117299bad0a54e6794e8c15
Vikas Salve <Vikas.Dsalve@hotmail.com>
2020-04-25 20:07:54 +0530

    40. Drop-down Lists

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using System.Data.Entity;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -24,7 +25,12 @@ namespace Vidly.Controllers
 
         public ActionResult New()
         {
-            return View();
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustmerViewModel
+            {
+                MembershipType = membershipTypes
+            };
+            return View(viewModel);
         }
             
         // GET: Customers
--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -24,6 +24,8 @@ namespace Vidly.Models
 
         public DbSet<Movie> Movies { get; set; }
 
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
