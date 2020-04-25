d3a64afc46998fa9d7f5c16a6b14670442724ff5
Vikas Salve <Vikas.Dsalve@hotmail.com>
2020-04-25 23:16:02 +0530

    53. Custom Validation

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -22,6 +22,7 @@ namespace Vidly.Models
         public byte MembershipTypeId { get; set; }
 
         [Display( Name = "Date of Birth")]
+        [Min18YearsIfMember]
         public DateTime? Birthdate { get; set; }
     }
 }
---
+++ Vidly/Models/Min18YearsIfMember.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId==0 || customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+
+            if (customer.Birthdate == null)
+                return new ValidationResult("Birthday is required");
+
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+
+            return (age >= 18)
+                ? ValidationResult.Success
+                : new ValidationResult("Customer should be at least 18 year Old an go Membership.");
+
+        }
+    }
+}
