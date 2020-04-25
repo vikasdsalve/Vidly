41b3f7e8c5fd608cec81aea616252d7742af2c75
Vikas Salve <Vikas.Dsalve@hotmail.com>
2020-04-25 11:02:08 +0530

    25. Code-first Migrations

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace Vidly.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customer { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
