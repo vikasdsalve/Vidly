4f34504e9c43d9e0c58f4e57dfc9aad518ad55bf
Vikas Salve <Vikas.Dsalve@hotmail.com>
2020-04-27 11:07:43 +0530

    104. Output Cache

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Vidly.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        //[OutputCache(Duration =50 , Location = OutputCacheLocation.Server,VaryByParam = "*")]
+        [OutputCache(Duration = 0 , VaryByParam = "*", NoStore =true)]
         public ActionResult Index()
         {
             return View();
