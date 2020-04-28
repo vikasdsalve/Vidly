36c0b906f8c18321186ace703b494ccbf7803edc
Vikas Salve <Vikas.Dsalve@hotmail.com>
2020-04-28 10:17:34 +0530

    Made somechnages

---
+++ Vidly/Models/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+
+
+namespace Vidly.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Customer> Customer { get; set; }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+
+        public DbSet<Genre> Genre { get; set; }
+
+        public DbSet<Rental> Rentals { get; set; }
+
+
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -27,28 +27,5 @@ namespace Vidly.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public DbSet<Customer> Customer { get; set; }
-
-        public DbSet<Movie> Movies { get; set; }
-
-        public DbSet<MembershipType> MembershipTypes { get; set; }
-
-        public DbSet<Genre> Genre { get; set; }
-
-        public DbSet<Rental> Rentals { get; set; }
-
-
-
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+ 
 }
